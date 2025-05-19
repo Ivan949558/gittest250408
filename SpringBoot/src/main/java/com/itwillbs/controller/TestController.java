174352b8d746eaa0f124c1022cde5c9388077238
Ivan949558 <dygks1407@naver.com>
2025-05-19 16:04:26 +0900

    수업도중 저장

--- SpringBoot/src/main/java/com/itwillbs/controller/TestController.java
+++ SpringBoot/src/main/java/com/itwillbs/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.itwillbs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.itwillbs.domain.MemberDTO;
 
 import lombok.extern.java.Log;
 
@@ -23,6 +27,26 @@ public class TestController {
 		//test2.jsp => 에러발생
 		return "test2";
 	}
+	
+	//http://localhost:8080/test3
+	//MemberDTO 객체 생성
+	//set 메서드 호출 "kim", "p123", "김길동"
+	//model 담아서("memberDTO", memberDTO)
+	//test3.html 이동
+	@GetMapping("/test3")
+	public String test3(Model model) {
+		
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId("kim");
+		memberDTO.setName("김길동");
+		memberDTO.setPasswd("p123");
+		
+		model.addAttribute("memberDTO", memberDTO);
+		
+		return "test3";
+	}
+	
+	
 	
 	
 }
---
+++ SpringBoot/src/main/java/com/itwillbs/domain/MemberDTO.java
@@ -0,0 +1,33 @@
+package com.itwillbs.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Lombok 관련 이노테이션(라이브러리)
+//@Setter, @Getter : Getter, Setter 메서드 생성
+//@ToString : toString() 메서드 생성
+//@ToString(exclude={변수명}) : 변수병 제외 toString() 메서드 생성
+//@NonNull : null 체크 => NullPointException 예외발생
+//@EqualsAndHashCode : equals(), hashCode() 메서드 생성
+//@Builder : 빌더 패턴 이용 객체 생성
+//@NoArgsConstructor : 파라미터가 없는 생성자(기본생성자) 생성
+//@AllArgsConsturctor : 모든 파라미터가 있는 생성자 생성
+//@RequiredArgsConstructor : 초기화 되지 않은 Final, @NonNull 붙은 필드 생성자 생성
+//@Value : 불변(값이 변하지 않음, 변경할 수 없는) 클래스 생성
+//@Data :  @Setter @Getter, @ToSring, 
+//		   @EqualsAndHashCode, @RequiredArgsConstructor 합친 어노테이션
+//@Log : log 자동변수 생성
+
+//회원관련 데이터를 저장해서 전달
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+	//객체지향 개념 특징 : 데이터 은닉(캡술화)
+	//멤버변수 : 외부에 접근 못하게 막아줌(데이터 은닉, 접근 제어자 private)
+	private String id;
+	private String passwd;
+	private String name;
+	
+}
